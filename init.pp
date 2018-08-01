acf8dc57b4bd674cf731ea7266ade88264f8dd93
root <root@eivindlia1.mylabserver.com>
2018-08-01 05:12:49 +0000

    fix some bugs

--- init.pp
+++ init.pp
@@ -4,11 +4,11 @@ class ntp(
   String $config_name         = $ntp::params::config_name,
   String $config_file_mode    = $ntp::params::config_file_mode,
   Array[String] $servers      = $ntp::params::servers,
-  String $service_ensure      = $ntp::service_ensure,
+  String $service_ensure      = $ntp::params::service_ensure,
   String $service_name        = $ntp::params::service_name,
-  Boolean $service_enable     = $ntp::params::servers_enable,
+  Boolean $service_enable     = $ntp::params::service_enable,
   Boolean $service_hasrestart = $ntp::params::service_hasrestart,
-  Boolean $service_hasstatus  = $ntp::params::hasstatus,
+  Boolean $service_hasstatus  = $ntp::params::service_hasstatus,
 ) inherits ::ntp::params {
   class { '::ntp::install': }
   -> class { '::ntp::config': }
